9e3d65652a8cc4dac52d89210efefc267cdb7641
MURALI <mummadimuralimohanreddy@gmail.com>
2024-03-04 09:20:11 +0530

    created the animations for login and sign up screens. Build app for home screen.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,4 +3,6 @@ class BaseUrls {
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAmdttJwLoYd3udbpr-zh1wlGvU3UKe1CA';
   static const signUpBaseUrl =
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAmdttJwLoYd3udbpr-zh1wlGvU3UKe1CA";
+  static const productsBaseUrl =
+      "https://shop-app-36d2c-default-rtdb.firebaseio.com/products/-Nrs9ukTjIqF1oqjCHVX/products.json";
 }
